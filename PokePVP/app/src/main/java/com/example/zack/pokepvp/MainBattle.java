f29d943567b2d3f05fe67565df0179d862346655
ZackMcClary34 <longhornzack@gmail.com>
2016-04-28 17:23:12 -0500

    Project :facepunch:

---
+++ PokePVP/app/src/main/java/com/example/zack/pokepvp/MainBattle.java
@@ -0,0 +1,61 @@
+package com.example.zack.pokepvp;
+
+import android.content.Context;
+import android.content.pm.ActivityInfo;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+import android.net.ConnectivityManager;
+import android.net.NetworkInfo;
+import android.os.AsyncTask;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.widget.ImageView;
+import android.widget.ProgressBar;
+import android.widget.TextView;
+
+import java.io.InputStream;
+import java.net.HttpURLConnection;
+import java.net.URL;
+
+public class MainBattle extends AppCompatActivity {
+    ProgressBar UserHealth;
+    ImageView UserImage;
+    TextView UserName;
+    ProgressBar EnemyHealth;
+    ImageView EnemyImage;
+    TextView EnemyName;
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main_battle);
+        setRequestedOrientation(ActivityInfo.SCREEN_ORIENTATION_LANDSCAPE);
+       // ConnectivityManager connMgr = (ConnectivityManager) getSystemService(Context.CONNECTIVITY_SERVICE);
+       // NetworkInfo networkInfo = connMgr.getActiveNetworkInfo();
+
+        //Initialize Globals
+
+        UserName = (TextView) findViewById(R.id.UserName);
+        UserHealth = (ProgressBar) findViewById(R.id.UserHealth);
+        UserImage = (ImageView) findViewById(R.id.UserImage);
+        EnemyName = (TextView) findViewById(R.id.EnemyName);
+        EnemyHealth = (ProgressBar) findViewById(R.id.EnemyHealth);
+        EnemyImage = (ImageView) findViewById(R.id.EnemyImage);
+
+
+        UserHealth.setMax(100);
+        UserHealth.setProgress(50);
+
+
+        EnemyHealth.setMax(100);
+        EnemyHealth.setProgress(25);
+        UserImage.setImageResource(R.mipmap.blastoise);
+        EnemyImage.setImageResource(R.mipmap.charizard);
+    }
+
+
+
+
+
+}
